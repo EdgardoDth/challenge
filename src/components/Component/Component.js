a6bab6d4fcffb3f3c77ba7a2e5ee91a8c5dd88c4
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 <edgardodth@gmail.com>
2023-06-21 17:43:35 -0600

    feat: :sparkles: creating components folder

---
+++ src/components/Component/Component.js
@@ -0,0 +1,169 @@
+import React, {useRef, useState} from "react";
+
+import Moveable from "react-moveable";
+
+const Component = ({
+  updateMoveable,
+  top,
+  left,
+  width,
+  height,
+  index,
+  color,
+  id,
+  setSelected,
+  isSelected = false,
+  updateEnd,
+  imagen,
+  fit,
+}) => {
+  const ref = useRef();
+
+  const [nodoReferencia, setNodoReferencia] = useState({
+    top,
+    left,
+    width,
+    height,
+    index,
+    color,
+    id,
+  });
+
+  let parent = document.getElementById("parent");
+  let parentBounds = parent?.getBoundingClientRect();
+
+  const onResize = async (e) => {
+    // check if resize is not out of bounds
+    if(e.clientY < parentBounds.y || e.clientY > parentBounds.height ||
+      parentBounds.width < e.clientX + e.boundingWidth)
+      return;
+    // ACTUALIZAR ALTO Y ANCHO
+    let newWidth = e.width;
+    let newHeight = e.height;
+
+    const positionMaxTop = top + newHeight;
+    const positionMaxLeft = left + newWidth;
+
+    if (positionMaxTop > parentBounds?.height)
+      newHeight = parentBounds?.height - top;
+    if (positionMaxLeft > parentBounds?.width)
+      newWidth = parentBounds?.width - left;
+
+    updateMoveable(id, {
+      top,
+      left,
+      width: newWidth,
+      height: newHeight,
+      color,
+      imagen,
+      fit,
+    });
+
+    // ACTUALIZAR NODO REFERENCIA
+    const beforeTranslate = e.drag.beforeTranslate;
+
+    ref.current.style.width = `${e.width}px`;
+    ref.current.style.height = `${e.height}px`;
+
+    let translateX = beforeTranslate[0];
+    let translateY = beforeTranslate[1];
+
+    ref.current.style.transform = `translate(${translateX}px, ${translateY}px)`;
+
+    setNodoReferencia({
+      ...nodoReferencia,
+      translateX,
+      translateY,
+      top: top + translateY < 0 ? 0 : top + translateY,
+      left: left + translateX < 0 ? 0 : left + translateX,
+    });
+  };
+
+  const onResizeEnd = async (e) => {
+    let newWidth = e.lastEvent?.width;
+    let newHeight = e.lastEvent?.height;
+
+    const positionMaxTop = top + newHeight;
+    const positionMaxLeft = left + newWidth;
+
+    if (positionMaxTop > parentBounds?.height)
+      newHeight = parentBounds?.height - top;
+    if (positionMaxLeft > parentBounds?.width)
+      newWidth = parentBounds?.width - left;
+
+    const { lastEvent } = e;
+    const { drag } = lastEvent;
+    const { beforeTranslate } = drag;
+
+    updateMoveable(
+      id,
+      {
+        top,
+        left,
+        width: newWidth,
+        height: newHeight,
+        color,
+        imagen,
+        fit,
+      },
+      true
+    );
+  };
+
+  return (
+    <>
+      <div
+        ref={ref}
+        className="draggable"
+        id={"component-" + id}
+        style={{
+          position: "absolute",
+          top: top,
+          left: left,
+          width: width,
+          height: height,
+          background: color,
+        }}
+        onClick={() => setSelected(id)}
+      >
+        <img
+          style={{
+            width: "100%",
+            height: "100%",
+            position: "absolute",
+            objectFit: fit,
+          }}
+          src={imagen}
+        />
+      </div>
+
+      <Moveable
+        target={isSelected && ref.current}
+        resizable
+        draggable
+        onDrag={(e) => {
+          updateMoveable(id, {
+            top: e.top,
+            left: e.left,
+            width,
+            height,
+            color,
+            imagen,
+            fit,
+          });
+        }}
+        onResize={onResize}
+        onResizeEnd={onResizeEnd}
+        keepRatio={false}
+        throttleResize={1}
+        renderDirections={["nw", "n", "ne", "w", "e", "sw", "s", "se"]}
+        edge={false}
+        zoom={1}
+        origin={false}
+        padding={{ left: 0, top: 0, right: 0, bottom: 0 }}
+      />
+    </>
+  );
+};
+
+export default Component;
